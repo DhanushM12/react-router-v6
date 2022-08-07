7cfbb5c4474229cd47eb8a84f9410b6ab9691e27
DhanushM12 <dhanushmukhi@gmail.com>
2022-08-07 13:22:28 +0530

    added v6 setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './Home';
+import Form from './Form';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <div className="App">
-     
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/form' element={<Form />}/>
+          <Route path='*'element={<NotFound />}/>
+        </Routes>
     </div>
   );
 }
